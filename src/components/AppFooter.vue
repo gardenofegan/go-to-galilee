3d9430ffdca915c43145f5f06b0139793b0923d7
Justin Egan <gardenofegan@gmail.com>
2024-03-19 07:16:42 -0400

    update footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="py-5 px-5">
-    <div class="d-flex justify-content-between align-items-md-center">
+    <div class="d-md-flex justify-content-between align-items-md-center">
       <div>
         <img src="/src/assets/img/Galilee_mobilemenu_logo_teal_193.png" class="footer-logo me-3" />
         <span class="text-white">©2024 Go To Galilee</span>
@@ -10,7 +10,8 @@
           <a href="#" class="text-white text-decoration-none">Resources</a>
           <span class="mx-2">|</span>
           <a href="#" class="text-white text-decoration-none">Download Galilee Card</a>
-          <span class="mx-2">|</span>
+          <span class="mx-2 d-none d-sm-inline">|</span>
+          <br class="d-sm-none d-inline"/>
           <router-link to="/support#contact-us" class="text-white text-decoration-none">Contact Us</router-link>
           <span class="mx-2">|</span>
           <router-link to="/support" class="text-white text-decoration-none">Support Galilee</router-link>
