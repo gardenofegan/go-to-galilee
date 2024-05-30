8bed0ada4b9b81503f56a7609e0574eac4c1dd72
Justin Egan <gardenofegan@gmail.com>
2024-05-29 20:19:00 -0400

    updates build process

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+import { fileURLToPath, URL } from "url";
+
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   build: {
     outDir: './docs'
-  },
-  server: {
-    port: 8080
   }
-})
+});
